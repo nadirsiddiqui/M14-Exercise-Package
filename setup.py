6832bfb4e9807b4ca49f540e94df632f9654b07e
Nadir Siddiqui <nadirsiddiqui@Nadirs-MacBook-Pro.local>
2022-07-11 23:45:52 -0400

    Added setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Demo',
+      version='0.1',
+      description='A pointless demonstration package',
+      url='https://github.com/nadirsiddiqui/M14-Exercise-Package',
+      author='Nadir Siddiqui',
+      author_email='nhs2bc@virginia.edu',
+      license='MIT',
+      packages=['demo'])
